614a422281ecf21e71d6006c6b96d5e662670a63
xl92@DESKTOP-F8KSG17 <xl92@mail.ru>
2023-04-08 09:53:53 +0300

    finish section ourClients

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,11 @@
 "use strict"
+
+$('.ourClients__left-bottom').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    });
